146b1ee74fcb050641df64bddf76558221618cc5
BIVA <mohosinaakter32810@gmail.com>
2025-05-04 12:37:37 +0600

    added a get method for fetch data

--- index.js
+++ index.js
@@ -1,12 +1,69 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // middleware
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipq1mm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+    
+    const gamerCollection = client.db('gamerDB').collection('gamer');
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    // id dhore data pawyar jonno 
+    app.get('/gamers/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await gamerCollection.findOne(query);
+      res.send(result);
+    })
+    
+// sob data pawyar jonno 
+    app.get('/gamers',async(req,res)=>{
+        const cursor = gamerCollection.find();
+        const result= await cursor.toArray();
+        res.send(result);
+    })
+// data create are jonno 
+    app.post('/gamers',async(req,res)=>{
+        const newGamer = req.body;
+        console.log(newGamer);
+        const result = await gamerCollection.insertOne(newGamer);
+        res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/',(req,res)=>{
